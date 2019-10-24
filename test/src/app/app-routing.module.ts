99b1a9b66209d19a0f542171d9a22fbb9e1783ca
dsychev <sda66@mail.ru>
2019-10-24 18:32:48 +0300

    ch1

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TestFormComponent} from "./features/test-form/test-form.component";
+import {HelpComponent} from "./features/help/help.component";
+import {lkComponent} from "./features/lk/lk.component"
+
 
 
 const routes: Routes = [
-  { path: 'testform', component: TestFormComponent },
+  { path: 'lk', component: lkComponent },
+  { path: 'help', component: HelpComponent }
 ];
 
 @NgModule({
