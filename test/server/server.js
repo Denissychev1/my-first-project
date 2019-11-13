a5d51b64fe307ad4098916c0dd5588272a687837
Kriosh <ivankrivosh.ik@gmail.com>
2019-11-13 14:19:20 +0300

    test

---
+++ test/server/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+function hello(request, response) {
+  console.log(request.url);
+  response.write("Hello, World! Author: Denis Sychev");
+  response.end();
+}
+
+http.createServer(hello).listen(1234, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server is in progress');
+  }
+});
+
