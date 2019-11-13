e2b0b65e746ca8cfd03b00b64e69c617da6090d4
Kriosh <ivankrivosh.ik@gmail.com>
2019-11-13 14:52:31 +0300

    test

--- test/server/server.js
+++ test/server/server.js
@@ -1,16 +1,35 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-function hello(request, response) {
-  console.log(request.url);
-  response.write("Hello, World! Author: Denis Sychev");
-  response.end();
-}
+const port = 2000;
 
-http.createServer(hello).listen(1234, err => {
-  if (err) {
+app.use('/api/users', async (req, res) => {
+  try {
+    res.status(200).json({
+      name : "User #1",
+      email : "email@gmai.com"
+    });
+  } catch (err) {
+    res.status(500).json(err);
     console.log(err);
-  } else {
-    console.log('Server is in progress');
   }
 });
 
+app.use('/api/imitations', async (req, res) => {
+  try {
+    res.status(200).json({
+      name : "Imitation #1"
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`App listening on port ${port}`);
+});
