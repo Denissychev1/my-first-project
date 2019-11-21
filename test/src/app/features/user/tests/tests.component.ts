f4af722e279ded644061df7b48becdd3a8643455
dsychev <sda66@mail.ru>
2019-11-21 21:00:48 +0300

    1

---
+++ test/src/app/features/user/tests/tests.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-tests',
+  templateUrl: './tests.component.html',
+  styleUrls: ['./tests.component.scss']
+})
+export class TestsComponent implements OnInit {
+  model: 1;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  clickButton() {
+    this.router.navigateByUrl('/user/addimitation');
+  }
+
+  clicButton() {
+    this.router.navigateByUrl('/user/admin');
+  }
+}
