20983d89d3ead17a4ec0384c00cf45356d400269
dsychev <sda66@mail.ru>
2019-11-25 21:17:27 +0300

    1

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule),
-
+    canActivate: [AuthGuard]
   },
   {
     path: 'user',
